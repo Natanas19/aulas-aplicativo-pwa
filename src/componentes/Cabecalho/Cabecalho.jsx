2856e6dfc1607e0cfceac9b4d4b43793ff93a832
Natanas19 <pro.natan98@gmail.com>
2026-03-10 01:34:52 -0300

    Primeiro commit

---
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import "./Cabecalho.css";
+
+function Cabecalho() {
+  return (
+    <header className="cabecalho__root">
+      <img src="/logo.png" height="32" alt="Logo" />
+      <div className="cabecalho__avatar">GT</div>
+    </header>
+  );
+}
+
+export default Cabecalho;
